9f8122b5e1ae7acb2f0cafaba393181e5d74eee5
Ktulhux <wilberhg@gmail.com>
2022-11-22 17:49:45 -0300

    last commit

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Alura Space</h1><p>Bem vindo ao espaço</p>")
+    return render(request, 'galeria/index.html')
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
